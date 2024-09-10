0bfaf55b404ce56c64c9ff8d330c5ab6d59b5150
Wazir Tatheer Abbas <baltiwazir7@gmail.comm>
2024-09-11 02:17:04 +0500

    Write a program that prints themultiplication table of a given number using a for loop.

---
+++ Assignment 4/Question 3.dart
@@ -0,0 +1,6 @@
+//Q.03: Write a program that prints themultiplication table of a given number using a for loop.
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    print('5*$i= ${5 * i}\n');
+  }
+}
