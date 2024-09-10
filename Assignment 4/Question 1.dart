2f45bc3d0b44c24baa14d11dd73908968344f06d
Wazir Tatheer Abbas <baltiwazir7@gmail.comm>
2024-09-11 02:07:35 +0500

    Write Dart code to check if the car is a sedan //and red in color. Print Match if both conditions are true, otherwise print No match

---
+++ Assignment 4/Question 1.dart
@@ -0,0 +1,11 @@
+//Q.01: Create a map named "car" with the following key-value pairs: "brand" as "Toyota",
+//"color" as "Red", "isSedan" as true. Write Dart code to check if the car is a sedan
+//and red in color. Print "Match" if both conditions are true, otherwise print "No match".
+void main() {
+  Map car = {'brand': 'Toyota', 'color': 'Red', 'isSedan': true};
+  if (car['color'] == 'Red' && car['isSedan'] == true) {
+    print('Match');
+  } else {
+    print('No match');
+  }
+}
