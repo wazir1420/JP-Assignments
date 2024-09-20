cacd571c022f65629a0b5d4dd7e9f235ad203e0f
Wazir Tatheer Abbas <baltiwazir7@gmail.comm>
2024-09-21 01:15:22 +0500

    done

---
+++ Assignment 5/Question12.dart
@@ -0,0 +1,10 @@
+void main() {
+  String myName = 'wazir';
+  int myNumber = 7898785;
+  String myEmail = 'wazir@gmail.com';
+  num myPercentage = 67;
+  print('My Name: $myName');
+  print('My Number: $myNumber');
+  print('My Email: $myEmail');
+  print('My Percentage: $myPercentage%');
+}
