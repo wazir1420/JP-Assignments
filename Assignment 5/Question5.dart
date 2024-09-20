22813cb3ab50d926cc58f3b4ae9dbd43dd2fd2af
Wazir Tatheer Abbas <baltiwazir7@gmail.comm>
2024-09-21 00:37:03 +0500

    Write a Dart program that takes a list of integers and sorts it in ascending order. Print the sorted list.

---
+++ Assignment 5/Question5.dart
@@ -0,0 +1,7 @@
+//Write a Dart program that takes a list of integers and sorts it in ascending order. Print the sorted list.
+
+void main() {
+  List<int> numbers = [5, 2, 8, 1, 3];
+  numbers.sort();
+  print('Sorted list: $numbers');
+}
