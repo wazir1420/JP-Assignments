54b59567b7ff26a74d6b7e5883825fdddadaef35
wazir1420 <wazirbalti1@gmail.com>
2024-09-25 20:47:21 -0700

    //Create a list of numbers & write a program to get the smallest & greatest number from a list. void main() {   List<int> numbers = [7, 6, 9, 4, 3, 0, 1];   numbers.sort();   print('The Smallest number is: ');   print('The Largest number is: '); }

--- Question4.dart
+++
@@ -1,7 +0,0 @@
-//Create a list of numbers & write a program to get the smallest & greatest number from a list.
-void main() {
-  List<int> numbers = [7, 6, 9, 4, 3, 0, 1];
-  numbers.sort();
-  print('The Smallest number is: ${numbers.first}');
-  print('The Largest number is: ${numbers.last}');
-}
