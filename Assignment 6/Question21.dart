3f8e0781d5ba2997a3d1ad3e0118cc5fee7b50e9
wazir1420 <wazirbalti1@gmail.com>
2024-09-25 23:42:29 -0700

    Write a Dart program that counts the number of digits in a given number using a while loop.

---
+++ Assignment 6/Question21.dart
@@ -0,0 +1,6 @@
+//Write a Dart program that counts the number of digits in a given number using a while loop.".
+void main() {
+  int number = 564657564;
+  int count = (number == 0) ? 1 : number.abs().toString().length;
+  print('Total digit in this number: $count');
+}
