acfba3305bd9b17b25e7d91d0dfa522edb45aec3
wazir1420 <wazirbalti1@gmail.com>
2024-09-25 23:48:44 -0700

    Given a map representing a shopping cart with keys as product names and values as quantities, write Dart code to check if a product named Apple exists in the cart. Print Product found if it exists, otherwise print Product not found.

---
+++ Assignment 6/Question22.dart
@@ -0,0 +1,12 @@
+//Given a map representing a shopping cart with keys as product
+//names and values as quantities, write Dart code to check if a
+//product named "Apple" exists in the cart. Print "Product found"
+//if it exists, otherwise print "Product not found".
+void main() {
+  Map Cart = {'Samsung': 54, 'Lenovo': 76, 'Mac': 79, 'Vivo': 90};
+  if (Cart.containsKey('Apple')) {
+    print('Product found');
+  } else {
+    print('Product not found');
+  }
+}
